7cbb742fc66d7dd2fee0f2325f0f0c71cf3f5862
Hemanth-A-H <hemanthhegde94@gmail.com>
2024-08-21 20:33:45 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,7 +1,12 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+extern BreachType Mock_classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+extern BreachType  mock_breach;
 
+TEST(TypeWiseAlertTestSuite,Test_check_and_alter) {
+Func_ptr_classifyTemperatureBreach = Mock_classifyTemperatureBreach;
+checkAndAlert(TO_CONTROLLER,PASSIVE_COOLING,-10)
+EXPECT_EQ(mock_breach,LOW);
 
 }

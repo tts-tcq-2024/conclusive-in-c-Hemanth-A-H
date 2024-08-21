fb9702a5d2921cc2cd29fac1afca185a76b344b4
Hemanth-A-H <hemanthhegde94@gmail.com>
2024-08-21 20:54:54 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -5,11 +5,28 @@ extern BreachType Mock_classifyTemperatureBreach(CoolingType coolingType, double
 extern BreachType  mock_breach;
 extern BreachType (*Func_ptr_classifyTemperatureBreach)(CoolingType, double);
 
-TEST(TypeWiseAlertTestSuite,Test_check_and_alter) {
+TEST(TypeWiseAlertTestSuite,Test_check_and_alter_LOW_breach) {
 Func_ptr_classifyTemperatureBreach = Mock_classifyTemperatureBreach;
 BreachType expected_breach = TOO_LOW;
 BatteryCharacter batteryChar = {PASSIVE_COOLING," "};
 checkAndAlert(TO_CONTROLLER,batteryChar,-10);
+EXPECT_EQ(mock_breach,expected_breach);
+
+}
+
+TEST(TypeWiseAlertTestSuite,Test_check_and_alter_High_breach) {
+Func_ptr_classifyTemperatureBreach = Mock_classifyTemperatureBreach;
+BreachType expected_breach = TOO_HIGH;
+BatteryCharacter batteryChar = {PASSIVE_COOLING," "};
+checkAndAlert(TO_CONTROLLER,batteryChar,50);
+EXPECT_EQ(mock_breach,expected_breach);
+}
+
+TEST(TypeWiseAlertTestSuite,Test_check_and_alter_Normal_breach) {
+Func_ptr_classifyTemperatureBreach = Mock_classifyTemperatureBreach;
+BreachType expected_breach = NORMAL;
+BatteryCharacter batteryChar = {PASSIVE_COOLING," "};
+checkAndAlert(TO_CONTROLLER,batteryChar,20);
 EXPECT_EQ(mock_breach,expected_breach);
 
 }
